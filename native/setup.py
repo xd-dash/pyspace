071a5d89566fd83d975096e1ce3816055dd665eb
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-15 21:33:47 -0700

    Remove disconnected native subpackage build

--- native/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import Extension, setup
-
-setup(
-    name="pyspace-gospace-native",
-    version="0.1.0",
-    ext_modules=[Extension("_gospace_native", ["_gospace_native.c"], libraries=["dl"])],
-)
